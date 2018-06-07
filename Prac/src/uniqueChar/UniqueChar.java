e5b796c36a5654508e292275396987826bd697a3
Hasec <dlwjd7789@naver.com>
2018-06-08 01:39:02 +0900

    유일한 문자검사

---
+++ Prac/src/uniqueChar/UniqueChar.java
@@ -0,0 +1,20 @@
+package uniqueChar;
+
+import java.util.Scanner;
+
+public class UniqueChar {
+	public static void main(String[] args) {
+		
+		String check = "Hello";
+		
+		char[] checkThis = check.toCharArray();
+		
+		for(int i=0;i<checkThis.length-1;i++) {
+			for(int j=i-1;j>=0;j--) {
+				if(checkThis[j]==checkThis[j+1]) {
+					System.out.println(j+"번째 문자와 "+(j+1)+"번째 문자가 같습니다");
+				}
+			}
+		}
+	}
+}
