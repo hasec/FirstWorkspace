947fa23c450908939121ee8abb56c365ae711da1
Hasec <dlwjd7789@naver.com>
2018-06-10 00:05:25 +0900

    변경

--- Prac/src/uniqueChar/UniqueChar.java
+++ Prac/src/uniqueChar/UniqueChar.java
@@ -12,7 +12,7 @@ public class UniqueChar {
 		for(int i=0;i<checkThis.length-1;i++) {
 			for(int j=i-1;j>=0;j--) {
 				if(checkThis[j]==checkThis[j+1]) {
-					System.out.println(j+"번째 문자와 "+(j+1)+"번째 문자가 같습니다");
+					System.out.println(j+"번째 문자와 "+(j+1)+"번째 문자가 같습니다 ");
 				}
 			}
 		}
