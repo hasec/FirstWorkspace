1029def08b0e8b59a06921fcc5b2b19829a810c2
Hasec <dlwjd7789@naver.com>
2018-06-16 14:17:09 +0900

    1. 중복건수중 최고값 출력 2. 중복문자 계산식을 +1이아닌 제곱으로 계산(초기값은3)

--- Prac/src/uniqueChar/UniqueChar.java
+++ Prac/src/uniqueChar/UniqueChar.java
@@ -1,20 +1,54 @@
 package uniqueChar;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 public class UniqueChar {
+
 	public static void main(String[] args) {
+		String check = "abcdaabebe";
+		HashMap<Character, int[][]> checkMap = new HashMap<Character, int[][]>();
+		List<Character> maxChar = new ArrayList<Character>();
+		int max = 0;
 		
-		String check = "Hello";
+		for(int i=0;i<check.length();i++) {
+			char key = check.charAt(i);
+			int[][] val = new int[1][2];
+			if(checkMap.containsKey(key)) {
+				int[][] tempval =checkMap.get(key);
+				val[0][0] = tempval[0][0]+1;
+				val[0][1] = tempval[0][1]*3;
+			}else {
+				val[0][0] = 1;
+				val[0][1] = 3;
+			}
+			if(val[0][0]>max) {
+				max = val[0][0];
+			}
+			checkMap.put(key, val);
+			//System.out.println(key+" "+val[0][0]+" "+val[0][1]);
+		}
 		
-		char[] checkThis = check.toCharArray();
-		
-		for(int i=0;i<checkThis.length-1;i++) {
-			for(int j=i-1;j>=0;j--) {
-				if(checkThis[j]==checkThis[j+1]) {
-					System.out.println(j+"ë²ˆì§¸ ë¬¸ìì™€ "+(j+1)+"ë²ˆì§¸ ë¬¸ìê°€ ê°™ìŠµë‹ˆë‹¤ ");
-				}
+		for(int i=0;i<check.length();i++) {
+			char key = check.charAt(i);
+			int[][] temp = checkMap.get(key);
+			if(temp[0][0]==max) {
+				maxChar.add(key);
 			}
 		}
+		
+		Set temp = new LinkedHashSet<Character>();
+		temp.addAll(maxChar);
+		maxChar.clear();
+		maxChar.addAll(temp);
+		
+		for(int i=0;i<maxChar.size();i++) {
+			System.out.print(maxChar.get(i)+" ");
+		}
+		System.out.print("°¡ "+max+"¹ø Áßº¹µÈ ¹®ÀÚ¿­ÀÔ´Ï´Ù");
+		
 	}
 }
